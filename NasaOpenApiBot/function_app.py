dda22a5567fa3b83536bbe1c535110ee0cffb393
gcmurillo <gcmurillo@outlook.com>
2023-01-27 11:54:05 -0500

    Add random-picture function

--- NasaOpenApiBot/function_app.py
+++ NasaOpenApiBot/function_app.py
@@ -1,8 +1,20 @@
 import azure.functions as func
+from nasa_api_manager import NasaApiManager
+
 
 app = func.FunctionApp()
+api_manager = NasaApiManager()
 
-@app.function_name(name="HttpTrigger1")
-@app.route(route="hello") # HTTP Trigger
-def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("HttpTrigger1 function processed a request!!!")
+@app.function_name(name="PictureOfTheDay")
+@app.route(route="apod") # HTTP Trigger
+def picture_of_the_day(req: func.HttpRequest) -> func.HttpResponse:
+    api_manager.get_apod_image()
+    return func.HttpResponse("APOD request processed!!!")
+
+
+@app.function_name(name="RandomPictures")
+@app.route(route="random-picture")
+def random_picture(req: func.HttpRequest) -> func.HttpResponse:
+    res = api_manager.get_random_pictures()
+    print(res.items)
+    return func.HttpResponse("Random Picture processed!")
