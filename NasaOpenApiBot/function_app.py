831970afd6cfd725c3a477536b4c43600a612e90
gcmurillo <gcmurillo@outlook.com>
2023-01-28 22:47:29 -0500

    Create tweets

--- NasaOpenApiBot/function_app.py
+++ NasaOpenApiBot/function_app.py
@@ -1,6 +1,6 @@
 import azure.functions as func
 from nasa_api_manager import NasaApiManager
-
+import utils
 
 app = func.FunctionApp()
 api_manager = NasaApiManager()
@@ -8,13 +8,14 @@ api_manager = NasaApiManager()
 @app.function_name(name="PictureOfTheDay")
 @app.route(route="apod") # HTTP Trigger
 def picture_of_the_day(req: func.HttpRequest) -> func.HttpResponse:
-    api_manager.get_apod_image()
+    res = api_manager.get_apod_image().__dict__
+    utils.create_APOD_tweet(res)
     return func.HttpResponse("APOD request processed!!!")
 
 
 @app.function_name(name="RandomPictures")
 @app.route(route="random-picture")
 def random_picture(req: func.HttpRequest) -> func.HttpResponse:
-    res = api_manager.get_random_pictures()
-    print(res.items)
+    res = api_manager.get_random_pictures().items[0]
+    utils.create_APOD_tweet(res)
     return func.HttpResponse("Random Picture processed!")
